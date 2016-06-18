7b4306d971b68746a5aa5244360ab9b1146d0a38
Josh Kuhar <jdkuhar@gmail.com>
2016-06-17 23:09:41 -0400

    update

--- app.js
+++ app.js
@@ -1,4 +1,36 @@
 
+/*
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Add Contacts</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1>Contacts</h1>
+<p id="test1">This is a paragraph.</p>
+<p id="test2">This is another paragraph.</p>
+
+<p>Input field: <input type="text" id="test3" value="Mickey Mouse"></p>
+
+<button id="btn1">Set Text</button>
+<button id="btn2">Set HTML</button>
+<button id="btn3">Set Value</button>
+<button id="btn4">Clear Field</button><br>
+<button id="btn5">-Empty-</button>
+<button id="btn6">-Remove-</button>
+<button id="btn7">--HTML--</button>
+<button id="btn8">Clear Field</button>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+	<script src="app.js"></script>
+</body>
+</html>
+
+
+
+
 $(document).ready(function(){
     $("#btn1").click(function(){
         $("#test1").text("Hello world!");
@@ -25,5 +57,26 @@ $(document).ready(function(){
     $("#btn8").click(function(){
         $("#test3").val("");
     });
-    
-});
+
+});*/
+/*
+
+
+
+
+
+  	</script>
+  
+  
+</body>
+</html>
+
+
+*/
+
+
+
+
+
+
+
